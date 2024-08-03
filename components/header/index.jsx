10666085f2de3184c4c5edcebf952b15cc8de528
João Bebber <joao.gbebber@gmail.com>
2024-08-03 17:09:01 -0300

    :tada: Initial commit

---
+++ components/header/index.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import Logo from "@/public/images/logo.svg"
+import styles from "./header.module.scss"
+
+const Header = () => {
+    return <div className={styles.container}>
+        <div className={styles.logotipo}>
+            <Image src={Logo} alt="Logo" />
+        </div>
+        <div className={styles.menu}>
+            <Link href="/">Home</Link>
+            <Link href="/">O que fazemos</Link>
+            <Link href="/">Cases</Link>
+        </div>
+        <div className={styles.action}>
+            <button className={styles.button}>Fale conosco</button>
+        </div>
+    </div>
+}
+
+export default Header
